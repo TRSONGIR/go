cf6f40a43fa02b828b92a9c9383932be0cd5563e
TRSONGIR <67713900+TRSONGIR@users.noreply.github.com>
2020-07-28 19:54:20 +0430

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,7 @@
 <?php
-/*$tr="http://sib-design.ir/sibmusic/demo2/?p=";
-$id= $_GET['url'];
-$url= $tr.$id;
-if($id != '' ){
-header("Location: $url");
-exit();
-}else{
-header("Location: http://doman.ir");
-exit();
-}*/
-if(isset($_GET['url'])){
-  $url= "http://sib-design.ir/sibmusic/demo2/?p=".$_GET['url'];
-  header("Location: $url");
-exit();}else{
-header("Location:http://trsong.ir")
-}
+$gdriveid = $_GET['url'];
+$gdrive= "https://drive.google.com/uc?id=";
+$url = $gdrive.$gdriveid;
+header ("Location: $url");
+exit;
 ?>
